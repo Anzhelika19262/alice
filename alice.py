bb9099c11c4c81259479f908b131232c3982cff8
Anjelika <ZAnzelika@ya.ru>
2020-04-27 20:11:30 +0300

    update

--- alice.py
+++ alice.py
@@ -81,5 +81,10 @@ def get_suggests(user_id):
 
 
 if __name__ == '__main__':
+<<<<<<< HEAD
     app.run()
     
+=======
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host='0.0.0.0', port=port)
+>>>>>>> master
